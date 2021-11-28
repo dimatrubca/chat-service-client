b9c9ea88f27f6bfc0c6390f2f1db2afc6a0c6ea2
Dmitri Trubca <dmitri.trubca@titanium-soft.com>
2021-11-28 18:38:49 +0200

    add basic socket io communication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import io from 'socket.io-client'
+
+let endPoint = 'http://localhost:5000'
+let socket = io.connect(`${endPoint}`)
+
+const App = () => {
+  const [messages, setMessages] = useState(['Hello and Welcome'])
+  const [room, setRoom] = useState("")
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    getMessages()
+  }, [messages.length])
+
+  
+
+  const getMessages = () => {
+    socket.on("message", msg => {
+      setMessages([...messages, msg]);
+    });
+  }
+
+  const onChange = e => {
+    setMessage(e.target.value)
+  }
+
+  const onClick = () => {
+    if (message !== '') {
+      socket.emit("message", message);
+      setMessage('');
+    } else {
+      alert('Please Add A Message');
+    }
+  };
+
+
+  const onClickRoom = () => {
+    if (room !== '') {
+      socket.emit('join', {
+        'user': 'username1',
+        'room': room
+      })
+    } else {
+      alert("Please enter room ID")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {messages.length > 0 &&
+        messages.map(msg => (
+          <div>
+            <p>{msg}</p>
+          </div>
+        ))}
+        <input value={room} name="room" onChange={e => setRoom(e.target.value)}/>
+        <button onClick={() => onClickRoom()}>Connect to room</button>
+        <input value={message} name="message" onChange={e => onChange(e)} />
+        <button onClick={() => onClick()}>Send Message</button>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
